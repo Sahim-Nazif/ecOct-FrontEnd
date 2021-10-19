d537f3c0bdaf63333f0a20445bb295e7115493a2
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-18 21:01:53 -0400

    created product reducer

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,18 @@
+
+export const getAllProductReducer=(state={}, action)=>{
+
+
+    switch (action.type) {
+        case 'GET_PRODUCTS_REQUEST':
+            return {loading:true, }
+    
+        case 'GET_PRODUCTS_SUCCESS':
+            return {loading:false, products:action.payload}
+
+        case 'GET_PRODUCTS_FAILED':
+            return {loading:false, error:action.payload}
+        
+        default: state
+        }
+
+}
