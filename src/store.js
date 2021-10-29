8aba293bab724d1ddddea6325345060d6cf5018b
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-10-28 20:11:28 -0400

    get cart item in local storage

--- src/store.js
+++ src/store.js
@@ -11,10 +11,16 @@ const reducer=combineReducers ({
     addToCartReducer:addToCartReducer
 })
 
+const cartItems= localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): []
+
+const initialState= {
+
+  addToCartReducer:{ cartItems: cartItems}
+}
 const composeEnhancers = composeWithDevTools({
     // Specify here name, actionsBlacklist, actionsCreators and other options
   });
-const store=createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+const store=createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)))
 
 
 export default store;
