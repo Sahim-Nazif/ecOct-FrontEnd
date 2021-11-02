3ba3f65300c5371416763d794252ec40c26963cb
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-11-01 21:20:21 -0400

    display added cart items intable

--- src/screens/Cartscreen.js
+++ src/screens/Cartscreen.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import {useState, useDispatch, useSelector} from 'react-redux'
+
 
 const Cartscreen = () => {
+
+    const cartReducerState= useSelector(state => state.addToCartReducer)
+    const {cartItems}= cartReducerState
     return (
         <div>
-            <h1>Cartscreen</h1>
+           <div className='row mt-3 justify-content-center'>
+                <div className='col-md-8'>
+                    <h3 className='m-4'>My Cart </h3>
+                    <table className='table table-bordered '>
+                  
+                    <thead >
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Remove</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {cartItems.map(item => {
+                                return (
+                                    <tr>
+                                        <td>{item.name}</td>
+                                        <td>${item.price}</td>
+                                        <td>{item.quantity}</td>
+                                        <td>${item.price * item.quantity}</td>
+                                         <td><i class="fas fa-trash"></i></td>
+                                     </tr>   
+                                    )
+                                })}
+                        </tbody>
+                      
+                    </table>
+
+                </div>
+           </div>
         </div>
     )
 }
