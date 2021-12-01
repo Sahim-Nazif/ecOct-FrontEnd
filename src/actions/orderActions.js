b6783f925f5a20001044ab96f64e5c07de60af38
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-11-30 20:30:55 -0500

    created action for order placing

---
+++ src/actions/orderActions.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+
+export const placeOrder=(token, cartTotal)=>(dispatch, getState)=>{
+
+    const currentUser=getState().loginReducer.currentUser
+    const cartItems=getState().cartReducer.cartItems
+
+    dispatch({type:'PLACE_ORDER_REQUEST'})
+
+    axios.post('/api/orders/placeOrder', {token, cartTotal, currentUser, cartItems}).then(res=>{
+        dispatch({type:'PLACE_ORDER_SUCCESS'})
+    }).catch(err=>{
+        dispatch({type:'PLACE_ORDER_FAILED'})
+    })
+}
